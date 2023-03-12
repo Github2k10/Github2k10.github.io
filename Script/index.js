1a386591c1bd5a2d2efbaad532b5d0dcd3ee1fa6
Ankit Kumar <65845325+Github2k10@users.noreply.github.com>
2023-03-12 14:21:57 +0530

    update

--- Script/index.js
+++ Script/index.js
@@ -1,16 +1,5 @@
-document.getElementById('resume-link-2').onclick = function() {
-    window.open("https://drive.google.com/file/d/1_PcGtcaBiErMtIao5uLzpASON5Kr4UL9/view?usp=sharing", "_blank");
-    // window.location.href = ".\data\Ankit-Kumar-Resume.pdf";
-}
-
-document.getElementById('git').onclick = function() {
-    window.open("https://github.com/Github2k10/", "_blank");
-}
-
-document.getElementById('linkin').onclick = function() {
-    window.open("https://www.linkedin.com/in/ankit-kumar-2k21/", "_blank");
-}
-
-document.getElementById('insta').onclick = function() {
-    window.open("https://www.instagram.com/___its.phoenix/", "_blank");
-}
+let resumedownload1 = document
+  .getElementById("resume-button-1")
+  .addEventListener("click", () => {
+    window.open("./data/Ankit-Kumar-Resume.pdf", "_blank");
+  });
