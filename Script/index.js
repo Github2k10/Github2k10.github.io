ace534c59a640cd57d0c187d71f8a861d1660a23
Ankit Kumar <65845325+Github2k10@users.noreply.github.com>
2023-05-04 15:50:31 +0530

    update

--- Script/index.js
+++ Script/index.js
@@ -4,8 +4,8 @@ let resumedownload1 = document
     window.open("./data/Ankit-Kumar-Resume.pdf", "_blank");
   });
 
-//   let resumedownload2 = document
-//   .getElementById("resume-button-2")
-//   .addEventListener("click", () => {
-//     window.open("./data/Ankit-Kumar-Resume.pdf", "_blank");
-//   });
+  let resumedownload2 = document
+  .getElementById("resume-button-2")
+  .addEventListener("click", () => {
+    window.open("./data/Ankit-Kumar-Resume.pdf", "_blank");
+  });
